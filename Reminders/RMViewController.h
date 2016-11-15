657ba92ff468e6ffc544ccd66d3b655a92a2ac40
riteshmiimobileapp <riteshsharma06@gmail.com>
2016-11-15 13:45:18 +0530

    reminders for private

---
+++ Reminders/RMViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMViewController.h
+//  Reminders
+//
+//  Created by SoftBunch  on 10/17/16.
+//  Copyright © 2016 miimobileapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMViewController : UIViewController
+
+@end
---
+++ Reminders/RMViewController.m
@@ -0,0 +1,37 @@
+//
+//  RMViewController.m
+//  Reminders
+//
+//  Created by SoftBunch  on 10/17/16.
+//  Copyright © 2016 miimobileapp. All rights reserved.
+//
+
+#import "RMViewController.h"
+
+@interface RMViewController ()
+
+@end
+
+@implementation RMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
